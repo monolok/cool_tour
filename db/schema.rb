6034dd248a2920393de38f06705d913ff86c0c7e
monolok <antoinebertin@me.com>
2015-02-27 23:01:37 +0100

    g model Language

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227215846) do
+ActiveRecord::Schema.define(version: 20150227220028) do
 
   create_table "guides", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20150227215846) do
 
   add_index "guides", ["email"], name: "index_guides_on_email", unique: true
   add_index "guides", ["reset_password_token"], name: "index_guides_on_reset_password_token", unique: true
+
+  create_table "languages", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "programs", force: :cascade do |t|
     t.string   "city"
